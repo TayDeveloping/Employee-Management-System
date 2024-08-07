a023055137994d59525790e8d1d1f7c5247ae1b8
tay <renegademisfit99@gmail.com>
2024-08-07 16:41:59 -0400

    added require variables

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+const inquirer = require('inquirer');
+const db = require('./db/db');
+
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Employee Role',
+                'Exit'
+            ],
+        },
+    ]).then(answer => {
+        switch (answer.action) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            default:
+                db.end();
+                break;
+        }
+    });
+};
+
 const viewAllDepartments = async () => {
     const res = await db.query('SELECT * FROM department');
     console.table(res.rows);
