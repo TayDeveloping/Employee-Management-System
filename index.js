4fa48197e5c5473f40f964ae3a9fbd3a0b7819c7
tay <renegademisfit99@gmail.com>
2024-08-07 21:21:18 -0400

    added dpeartment and role, created functions for adding roles and employees in index.js

--- index.js
+++ index.js
@@ -54,3 +54,98 @@ const viewAllDepartments = async () => {
     mainMenu();
 };
 
+const viewAllRoles = async () => {
+    const res = await db.query('SELECT * FROM role');
+    console.table(res.rows);
+    mainMenu();
+};
+
+const viewAllEmployees = async () => {
+    const res = await db.query('SELECT * FROM employee');
+    console.table(res.rows);
+    mainMenu();
+};
+
+const addDepartment = async () => {
+    const answer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the department name:',
+        },
+    ]);
+    await db.query('INSERT INTO department (name) VALUES ($1)', [answer.name]);
+    console.log(`Added ${answer.name} to the database.`);
+    mainMenu();
+};
+
+const addRole = async () => {
+    const departments = await db.query('SELECT * FROM department');
+    const departmentChoices = departments.rows.map(department => ({
+        name: department.name,
+        value: department.id,
+    }));
+    const answer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the role title:',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter the role salary:',
+        },
+        {
+            type: 'list',
+            name: 'department_id',
+            message: 'Select the department for this role:',
+            choices: departmentChoices,
+        },
+    ]);
+    await db.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [answer.title, answer.salary, answer.department_id]);
+    console.log(`Added ${answer.title} to the database.`);
+    mainMenu();
+};
+
+const addEmployee = async () => {
+    const roles = await db.query('SELECT * FROM role');
+    const roleChoices = roles.rows.map(role => ({
+        name: role.title,
+        value: role.id,
+    }));
+    const employees = await db.query('SELECT * FROM employee');
+    const managerChoices = employees.rows.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id,
+    }));
+    managerChoices.unshift({ name: 'None', value: null });
+
+    const answer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter the employee first name:',
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter the employee last name:',
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Select the employee role:',
+            choices: roleChoices,
+        },
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Select the employee manager:',
+            choices: managerChoices,
+        },
+    ]);
+    await db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [answer.first_name, answer.last_name, answer.role_id, answer.manager_id]);
+    console.log(`Added ${answer.first_name} ${answer.last_name} to the database.`);
+    mainMenu();
+};
