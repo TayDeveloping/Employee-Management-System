8794c34a3f7de8012d6f09f919dae35be1b2c71e
tay <renegademisfit99@gmail.com>
2024-08-08 17:01:13 -0400

    edited end of index.js issue and reimported package.json

--- index.js
+++ index.js
@@ -149,3 +149,36 @@ const addEmployee = async () => {
     console.log(`Added ${answer.first_name} ${answer.last_name} to the database.`);
     mainMenu();
 };
+
+const updateEmployeeRole = async () => {
+    const employees = await db.query('SELECT * FROM employee');
+    const employeeChoices = employees.rows.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id,
+    }));
+    const roles = await db.query('SELECT * FROM role');
+    const roleChoices = roles.rows.map(role => ({
+        name: role.title,
+        value: role.id,
+    }));
+
+    const answer = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee_id',
+            message: 'Select the employee to update:',
+            choices: employeeChoices,
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Select the new role for this employee:',
+            choices: roleChoices,
+        },
+    ]);
+    await db.query('UPDATE employee SET role_id = $1 WHERE id = $2', [answer.role_id, answer.employee_id]);
+    console.log(`Updated employee role in the database.`);
+    mainMenu();
+};
+
+mainMenu();
